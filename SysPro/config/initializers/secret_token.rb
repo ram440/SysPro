5a67de4504a2ec7e0d011044d0debed646fe4e7c
omkar <omkar.b440@gmail.com>
2012-09-04 00:08:35 +0530

    my changes

---
+++ SysPro/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SysPro::Application.config.secret_token = '34350478400e990af8bf1787073eaf02a834b27433addfcf7ad97d24ed075edb318bbb996dbc2ed9817505de5577b49d1968e144ab7bb5c8c1dd8201f4cb014c'
